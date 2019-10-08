b67b8c8c1dd56fad717e490b66b6af9a28cf71ad
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2019-10-08 11:20:30 +0530

    Establing best practice time out enviroment veriable

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const mongodb = require("mongodb");
 
-const connectionString =
-  "mongodb+srv://webans:13a11a0501@cluster0-eya59.mongodb.net/social_app_data?retryWrites=true&w=majority";
 mongodb.connect(
-  connectionString,
+  process.env.CONNECTIONSTRING,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err, client) => {
     module.exports = client.db();
     const app = require("./app");
-    app.listen(3000);
+    app.listen(process.env.PORT);
   }
 );
